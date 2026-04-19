eb6c10096a435a57cc864b29a88926a126c0024b
passaelen <pas.saelen@gmail.com>
2026-04-19 19:32:20 +0200

    Update data.js

--- data.js
+++ data.js
@@ -3,8 +3,24 @@ const DATA = {
     suisse: 338.66,
     belgique: 130.62
   },
+
   silver: {
     suisse: 6000
   },
-  
+
+  portfolio: {
+    or_suisse_g: 338.66,
+    or_belgique_g: 130.62,
+    argent_g: 6000,
+
+    or_suisse_cost: 19722.90,
+    or_belgique_cost: 8375.39,
+    argent_cost: 3379.88
+  },
+
+  etf: {
+    monde: { parts: 5, buy: 761.06 },
+    seniors: { parts: 13, buy: 1048.57 },
+    juniors: { parts: 0, buy: 0 }
+  }
 };
