87befef91ed281b47dee6d307ac2d9d38fe04f50
passaelen <pas.saelen@gmail.com>
2026-04-27 08:35:25 +0200

    Delete colors.js

--- colors.js
+++
@@ -1,9 +0,0 @@
-const COLORS = {
-    or_suisse: "#facc15",
-    or_belgique: "#f59e0b",
-    argent: "#9ca3af",
-
-    etf_monde: "#38bdf8",
-    etf_seniors: "#22c55e",
-    etf_juniors: "#64748b"
-};
