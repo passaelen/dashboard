aeda84b01f0915100a092fcb03fdb8d5649c7e0d
passaelen <pas.saelen@gmail.com>
2026-04-28 08:05:00 +0200

    Create achats.js

---
+++ achats.js
@@ -0,0 +1,10 @@
+const achats = [
+ {date: "2026-04-27", type: "ETF_MONDE", montant: 471.58, parts: 3, label: "3 ETF Monde"},
+ {date: "2026-03-27", type: "ETF_MONDE", montant: 145.36, parts: 1, label: "1 ETF Monde"},
+ {date: "2026-03-02", type: "ETF_MONDE", montant: 154.14, parts: 1, label: "1 ETF Monde"},
+ {date: "2026-03-02", type: "ETF_MONDE", montant: 154.20, parts: 1, label: "1 ETF Monde"},
+ {date: "2026-02-25", type: "ETF_MONDE", montant: 155.11, parts: 1, label: "1 ETF Monde"}, 
+ {date: "2026-02-16", type: "ETF_MONDE", montant: 152.25, parts: 1, label: "1 ETF Monde"}, 
+  
+ {date: "2026-01-19", type: "OR", montant: 2250.60, poids: 15.55, label: "5x 1/10 Philharmonique"}
+];
