e455a9dfb9efef4486ee15f2f6c99c92c2d251d4
passaelen <pas.saelen@gmail.com>
2026-04-28 10:27:41 +0200

    Update achats.js

--- achats.js
+++ achats.js
@@ -1,3 +1,7 @@
+const metaux = [
+  {date: "19/01/2026", type: "OR", montant: 2250.60, poids: 15.55, label: "5x 1/10 Philh."}
+];
+
 const achats = [
  {date: "27/04/2026", type: "ETF_MONDE", montant: 471.58, parts: 3, label: "3 ETF Monde"},
  {date: "27/03/2026", type: "ETF_MONDE", montant: 145.36, parts: 1, label: "1 ETF Monde"},
@@ -5,8 +9,5 @@ const achats = [
  {date: "02/03/2026", type: "ETF_MONDE", montant: 154.20, parts: 1, label: "1 ETF Monde"},
  {date: "25/02/2026", type: "ETF_MONDE", montant: 155.11, parts: 1, label: "1 ETF Monde"}, 
  {date: "16/02/2026", type: "ETF_MONDE", montant: 152.25, parts: 1, label: "1 ETF Monde"}, 
-
- {date: "20/03/2026", type: "ETF_SENIOR", montant: 1048.57, parts: 13, label: "13 ETF SENIORS"}, 
- 
- {date: "19/01/2026", type: "OR", montant: 2250.60, poids: 15.55, label: "5x 1/10 Philh."}
-];
+ {date: "20/03/2026", type: "ETF_SENIOR", montant: 1048.57, parts: 13, label: "13 ETF SENIORS"} 
+ ];
