92ab795c49341f074f9f9636281035f74acbae45
passaelen <pas.saelen@gmail.com>
2026-04-28 13:11:19 +0200

    Update achats.js

--- achats.js
+++ achats.js
@@ -9,5 +9,5 @@ const achats = [
  ]
 
 const metaux = [
-  {date: "19/01/2026", type: "OR", loc: "Bel.", tot: 2250.60, poids: 15.55, label: "1/10 Philh."}
+  {date: "19/01/2026", type: "OR", loc: "Bel.", tot: 2250.60, poids: 15.55, label: "⅒ Philh."}
 ];
