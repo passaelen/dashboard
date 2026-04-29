e8354ead37f454ca75495c56875ebf8eb4ce5f28
passaelen <pas.saelen@gmail.com>
2026-04-29 13:51:49 +0200

    Update achats.js

--- achats.js
+++ achats.js
@@ -41,6 +41,7 @@ const metaux = [
   {date: "29/03/2020", type: "OR", loc: "Suisse", tot: 0.0, poids: 10.00, label: "L. 2.5g. Pamp"},
   {date: "29/03/2020", type: "OR", loc: "Suisse", tot: 0.0, poids: 4.00, label: "L. 1g. Pamp"},
   {date: "29/03/2020", type: "OR", loc: "Suisse", tot: 0.0, poids: 2.00, label: "L. 1g. Pamp"},
-  {date: "29/03/2020", type: "OR", loc: "Suisse", tot: 0.0, poids: 15.55, label: "P. ⅒ Philh."}
+  {date: "29/03/2020", type: "OR", loc: "Suisse", tot: 0.0, poids: 15.55, label: "P. ⅒ Philh."},
+  {date: "29/03/2020", type: "ARGENT", loc: "Suisse", tot: 3379.88, poids: 6000.0, label: "L. 1Kg."}
  ];
 
