7c465e9d79690b76a39fa61a543379a3a0846741
passaelen <pas.saelen@gmail.com>
2026-05-03 13:25:35 +0200

    Update birthday.js

--- birthday.js
+++ birthday.js
@@ -16,6 +16,11 @@ function launchFireworks(){
 
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  canvas.style.position = "fixed";
+  canvas.style.top = 0;
+  canvas.style.left = 0;
+  canvas.style.pointerEvents = "none";
+  canvas.style.zIndex = 9998;
 
   const particles = [];
 
