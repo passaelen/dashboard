e7412b03d2b2e25f5c959aa9707a90647e19e737
passaelen <pas.saelen@gmail.com>
2026-06-25 14:22:55 +0200

    Update data.js

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ const DATA = {
     juniors: { parts: 0, buy: 0 },
     or: { parts: 1, buy: 354.22},
     argent: { parts: 7, buy: 369.09},
-    petrole: { parts: 0, buy: 0}
+    petrole: { parts: 5, buy: 242.62}
   }
 };
 
